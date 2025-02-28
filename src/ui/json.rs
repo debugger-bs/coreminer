8646e44b7a57f1311b67b9b53af02ef6ea7af09f
Christoph J. Scherr <software@cscherr.de>
2025-02-28 11:56:23 +0100

    feat: JsonUI::process outputs the serialized Feedback #19 #16

--- src/ui/json.rs
+++ src/ui/json.rs
@@ -1,3 +1,5 @@
+use serde_json::json;
+
 use crate::errors::Result;
 
 use super::DebuggerUI;
@@ -6,13 +8,13 @@ pub struct JsonUI {}
 
 impl JsonUI {
     pub fn build() -> Result<Self> {
-        todo!()
+        Ok(JsonUI {})
     }
 }
 
 impl DebuggerUI for JsonUI {
     fn process(&mut self, feedback: crate::feedback::Feedback) -> Result<super::Status> {
-        // println!("{}", serde_json::to_string(&feedback)?);
+        println!("{}", json!({ "feedback": feedback }));
 
         todo!()
     }
