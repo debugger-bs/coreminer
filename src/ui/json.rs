359d5d560568e1f10de3e73ae9cb18930c755e9c
Christoph J. Scherr <software@cscherr.de>
2025-02-28 11:12:28 +0100

    feat: add basic json interface

---
+++ src/ui/json.rs
@@ -0,0 +1,19 @@
+use crate::errors::Result;
+
+use super::DebuggerUI;
+
+pub struct JsonUI {}
+
+impl JsonUI {
+    pub fn build() -> Result<Self> {
+        todo!()
+    }
+}
+
+impl DebuggerUI for JsonUI {
+    fn process(&mut self, feedback: crate::feedback::Feedback) -> Result<super::Status> {
+        println!("{}", serde_json::to_string(&feedback)?);
+
+        todo!()
+    }
+}
