d7ac6b2abe552601db85ace00e7808f38fb4cdf3
Christoph J. Scherr <software@cscherr.de>
2025-02-28 12:18:25 +0100

    feat: impl a basic JsonUI::process #16

--- src/ui/json.rs
+++ src/ui/json.rs
@@ -1,4 +1,7 @@
+use std::io::BufReader;
+
 use serde_json::json;
+use tracing::error;
 
 use crate::errors::Result;
 
@@ -16,6 +19,15 @@ impl DebuggerUI for JsonUI {
     fn process(&mut self, feedback: crate::feedback::Feedback) -> Result<super::Status> {
         println!("{}", json!({ "feedback": feedback }));
 
-        todo!()
+        let mut reader = BufReader::new(std::io::stdin());
+        loop {
+            match serde_json::from_reader(&mut reader) {
+                Ok(a) => return Ok(a),
+                Err(e) => {
+                    error!("{e}");
+                    continue;
+                }
+            }
+        }
     }
 }
