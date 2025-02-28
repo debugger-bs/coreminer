fbde8a831dfb69308e28ea22703aa5e7a4f9126d
Christoph J. Scherr <software@cscherr.de>
2025-02-28 11:26:40 +0100

    chore: JsonUI::process is still a todo

--- src/ui/json.rs
+++ src/ui/json.rs
@@ -12,7 +12,7 @@ impl JsonUI {
 
 impl DebuggerUI for JsonUI {
     fn process(&mut self, feedback: crate::feedback::Feedback) -> Result<super::Status> {
-        println!("{}", serde_json::to_string(&feedback)?);
+        // println!("{}", serde_json::to_string(&feedback)?);
 
         todo!()
     }
